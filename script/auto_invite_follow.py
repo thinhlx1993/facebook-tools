47e8340da09aa86076c1adfb55c3bbc1bc3be06c
ThinhPC <thinhlx1993@gmail.com>
2021-06-17 13:15:19 +0700

    save

--- script/auto_invite_follow.py
+++ script/auto_invite_follow.py
@@ -92,22 +92,21 @@ def paste_text(inp_text):
 
 if __name__ == '__main__':
     number_invited = 0
-    default_region = (800, 0, 70, 800)
-    while number_invited < 150:
-        if waiting_for("like.PNG", region=default_region, waiting_time=10):
-            click_to("like.PNG", region=default_region)
+    while number_invited < 200:
+        if check_exist("like.PNG", region=(850, 200, 70, 800)):
+            click_to("like.PNG", region=(850, 200, 70, 800))
 
             waiting = 0
             while waiting <= 3:
-                number_invited += click_many("invite.PNG", region=(800, 0, 600, 800))
+                number_invited += click_many("invite.PNG", region=(850, 200, 600, 800))
                 pyautogui.moveTo(x=1091, y=573, duration=0.5)
                 pyautogui.scroll(-400)
                 pyautogui.scroll(-400)
                 time.sleep(1)
-                if not check_exist("invite.PNG", region=default_region):
+                if not check_exist("invite.PNG", region=(850, 200, 600, 800)):
                     waiting += 1
-            if check_exist("close_invite.PNG", region=default_region):
-                click_to("close_invite.PNG", region=default_region)
+            if check_exist("close_invite.PNG", region=(850, 200, 600, 800)):
+                click_to("close_invite.PNG", region=(850, 200, 600, 800))
         pyautogui.moveTo(x=1635, y=655, duration=0.5)
         pyautogui.click(x=1635, y=655)
         pyautogui.scroll(-1500)
