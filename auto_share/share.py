e58daaadc1860c7d59e2ba81ad9e6f1ab56b5016
ThinhPC <thinhlx1993@gmail.com>
2021-06-19 13:32:49 +0700

    save

--- auto_share/share.py
+++ auto_share/share.py
@@ -12,7 +12,7 @@ if __name__ == '__main__':
     # scheduler = scheduler_table.find_one({"shared": False, "scheduler_time": {"$lte": datetime.now().timestamp()}})
     # if scheduler:
     # scheduler_table.update_one({"_id": scheduler['_id']}, {"$set": {"shared": True}})
-    video_id = "818392472129678"
+    video_id = "320460456213056"
     print(f"share video {video_id}")
     time.sleep(2)
     pyautogui.hotkey('winleft', 'd')
@@ -29,30 +29,29 @@ if __name__ == '__main__':
 
         for i in range(60):
             time.sleep(1)
-            playbtn = check_exist("playbtn.PNG", confidence=0.8)
+            playbtn = check_exist("playbtn.PNG", confidence=0.85)
             if playbtn:
                 pyautogui.moveTo(playbtn)
                 pyautogui.click(playbtn)
             pyautogui.moveTo(relative_position(1027, 549), duration=1)
-            playbtn = check_exist("play_btn_2.PNG", confidence=0.8)
+            playbtn = check_exist("play_btn_2.PNG", confidence=0.85)
             if playbtn:
                 pyautogui.moveTo(playbtn)
                 pyautogui.click(playbtn)
 
         buttons = ['share_btn_1.PNG', 'share_btn.PNG']
         share_x, share_y, idx = deciscion(buttons)
-        pyautogui.click(share_x, share_y)
-        click_to("options.PNG", confidence=0.9, interval=2, waiting_time=15)
-        click_to("share_to_group.PNG", confidence=0.9, interval=2)
+        pyautogui.click(share_x, share_y, interval=2)
+        click_to("options.PNG", confidence=0.95, interval=2, waiting_time=15)
+        click_to("share_to_group.PNG", confidence=0.95, interval=2)
 
         pyautogui.moveTo(relative_position(1027, 549), duration=1)
-        share_box_x, share_box_y = relative_position(x=707, y=378)
-        relative_w, relative_h = relative_position(x=1525, y=894)
+        # share_box_x, share_box_y = relative_position(x=707, y=378)
+        # relative_w, relative_h = relative_position(x=1525, y=894)
         if random.choice([0, 1]) == 0:
             pyautogui.scroll(-200)
 
-        groups = pyautogui.locateAllOnScreen(f"btn/public_group.PNG", confidence=0.8,
-                                             region=(share_box_x, share_box_y, relative_w, relative_h))
+        groups = pyautogui.locateAllOnScreen(f"btn/public_group.PNG", confidence=0.85)
         groups = list(groups)
         group = random.choice(groups)
         pyautogui.click(group, duration=2)
@@ -66,4 +65,5 @@ if __name__ == '__main__':
             spam = waiting_for("spam.PNG", confidence=0.9, waiting_time=10)
             if spam:
                 pyautogui.hotkey('ctrl', 'w')
+                pyautogui.press('enter')
         # time.sleep(7200)
