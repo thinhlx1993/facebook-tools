11a782c81aede4feb70cd228676900f021a771fb
ThinhPC <thinhlx1993@gmail.com>
2021-06-20 00:43:08 +0700

    save

--- ffmpeg/crop_speed_up_1.13_change_audio.bat
+++ ffmpeg/crop_speed_up_1.13_change_audio.bat
@@ -13,6 +13,5 @@ Echo "%rand%"
 
 
 for %%f in (*.avi *.flv *.mkv *.mpg *.mp4) do (
-	ffmpeg -i "%%f" -i "%SrcDir%\%rand%.mp3" -c:v copy -c:a aac -b:a 256k -map "0:0" -map "1:0" -shortest crop/"%%f" -y
-	ffmpeg -i crop/"%%f" -vf "setpts=0.87*PTS,crop=in_w*85/100:in_h*85/100,scale=420:420" done/"%%f" -y
+	ffmpeg -i "%%f" -i "%SrcDir%\%rand%.mp3" -vf "setpts=0.87*PTS,crop=in_w*85/100:in_h*85/100" -map "0:0" -map "1:0" -shortest done/"%%f" -y
 )
