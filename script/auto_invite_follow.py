f961cd3f8bafc071da05ce054f51ee049d2c1fdb
ThinhPC <thinhlx1993@gmail.com>
2021-06-26 16:39:07 +0700

    save

--- script/auto_invite_follow.py
+++ script/auto_invite_follow.py
@@ -95,13 +95,13 @@ if __name__ == '__main__':
     while number_invited < 200:
         if check_exist("like.PNG", region=(850, 0, 400, 1000)):
             click_to("like.PNG", region=(850, 0, 400, 1000))
-
+            waiting_for("close_invite.PNG", region=(850, 200, 800, 800))
             waiting = 0
             while waiting <= 3:
                 number_invited += click_many("invite.PNG", region=(850, 200, 800, 800))
                 pyautogui.moveTo(x=1091, y=573, duration=0.5)
-                pyautogui.scroll(-400)
-                pyautogui.scroll(-400)
+                pyautogui.scroll(-200)
+                pyautogui.scroll(-200)
                 time.sleep(1)
                 if not check_exist("invite.PNG", region=(850, 200, 800, 800)):
                     waiting += 1
@@ -109,6 +109,6 @@ if __name__ == '__main__':
                 click_to("close_invite.PNG", region=(850, 200, 800, 800), confidence=0.95)
         pyautogui.moveTo(x=1635, y=655, duration=0.5)
         pyautogui.click(x=1635, y=655)
-        pyautogui.scroll(-1500)
-        pyautogui.scroll(-1000)
+        pyautogui.scroll(-800)
+        pyautogui.scroll(-800)
         time.sleep(1)
