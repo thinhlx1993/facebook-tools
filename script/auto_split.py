24c622ab141fc4a2e1961f70e15ec06e692dcedf
ThinhPC <thinhlx1993@gmail.com>
2021-07-01 07:19:30 +0700

    save

--- script/auto_split.py
+++ script/auto_split.py
@@ -44,7 +44,7 @@ if __name__ == '__main__':
     # Create a VideoCapture object and read from input file
     # If the input is the camera, pass 0 instead of the video file name
     file_name = "Oddly Satisfying Videos"
-    cap = cv2.VideoCapture('downloaded/Daily Dose of Satisfaction/1 Hour Oddly Satisfying Videos- Calming with Satisfy Relaxing Music--8UzIv3ljxc.mkv')
+    cap = cv2.VideoCapture('images/1 Hour Oddly Satisfying Videos- Calming with Satisfy Relaxing Music.mkv')
     fps = cap.get(cv2.CAP_PROP_FPS)
 
     prev_frame = None
@@ -57,8 +57,9 @@ if __name__ == '__main__':
     frame_height = int(cap.get(4))
 
     # Define the codec and create VideoWriter object.The output is stored in 'outpy.avi' file.
-    start_index = 0
+    start_index = 20
     file_out = f'images/{file_name} Episode {start_index + 1}.avi'
+    print(f"file out {file_out}")
     out = cv2.VideoWriter(file_out, cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'), fps, (frame_width, frame_height))
 
     scale_percent = 25  # percent of original img size
