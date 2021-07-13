11e5a8bd6358dfa0048af99000b90b15f98935d8
ThinhPC <thinhlx1993@gmail.com>
2021-07-13 22:55:24 +0700

    save

--- script/auto_invite_group.py
+++ script/auto_invite_group.py
@@ -1,7 +1,7 @@
 import sys
 import time
 import pyautogui
-from utils import logger, click_to, click_many, check_exist, waiting_for
+from utils import logger, click_to, click_many, check_exist, waiting_for, check_exit_program
 import pytesseract
 # pip install paddlepaddle==2.0.0 -i https://mirror.baidu.com/pypi/simple
 import keyboard  # using module keyboard
@@ -14,15 +14,18 @@ def inviting():
         click_many("check_box.PNG", confidence=0.98)
         pyautogui.moveTo(1035, 751)
         pyautogui.scroll(-700)
+        check_exit_program()
         da_chon_btn = check_exist("da_chon.PNG", confidence=.7)
         if da_chon_btn:
             x, y, w, h = da_chon_btn
             img = pyautogui.screenshot(region=(x - 150, y, 350, 20))
             # img.show()
             # custom_config = r'--oem 3 --psm 6'
+            check_exit_program()
             texts = pytesseract.image_to_string(img)
             if texts:
                 for text in texts.split(' '):
+                    check_exit_program()
                     try:
                         text = text.strip()
                         number_invited = int(text)
