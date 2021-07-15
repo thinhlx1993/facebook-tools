edc66aab0e85e2e2690ae6c6c90b466894be3724
ThinhPC <thinhlx1993@gmail.com>
2021-07-15 15:51:51 +0700

    save

--- ffmpeg/crop_speed_mix_overwrite_audio_ban_quyen/overwrite_audio_the_rock_city.bat
+++ ffmpeg/crop_speed_mix_overwrite_audio_ban_quyen/overwrite_audio_the_rock_city.bat
@@ -4,5 +4,5 @@ if not exist "done" mkdir ""done" "
 
 
 for %%f in (*.avi *.flv *.mkv *.mpg *.mp4) do (
-  ffmpeg -i %%f -i "C:\RockTheCity.mp3" -map "0:0" -map "1:0" -preset ultrafast -crf 24 -shortest done/%1 -y
+  ffmpeg -i "%%f" -i "C:\RockTheCity.mp3" -map "0:0" -map "1:0" -preset ultrafast -crf 24 -shortest done/"%%f" -y
 )
