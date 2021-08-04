5b4d8beca6027860c0e93efdbdc943ac8c4c6f9b
ThinhPC <thinhlx1993@gmail.com>
2021-08-04 18:24:46 +0700

    save

--- script/auto_trim.py
+++ script/auto_trim.py
@@ -1,8 +1,9 @@
-import datetime
 import os
-
-from image_similarity_measures.quality_metrics import ssim
 import cv2
+import datetime
+import subprocess
+import numpy as np
+from image_similarity_measures.quality_metrics import ssim
 
 
 if __name__ == '__main__':
@@ -10,16 +11,18 @@ if __name__ == '__main__':
     # and the original + photoshop
     # Create a VideoCapture object and read from input file
     # If the input is the camera, pass 0 instead of the video file name
-    root_folder = """images/Wood Youtube"""
+    root_folder = """split"""
     for file in os.listdir(root_folder):
         file_ext = os.path.splitext(file)[1]
-        if file_ext != '.mkv':
+        if file_ext != '.mp4':
             continue
-
-        cap = cv2.VideoCapture(f"{root_folder}/{file}")
+        filename = f"{root_folder}/{file}"
+        print(f"process file {filename}")
+        cap = cv2.VideoCapture(filename)
         fps = cap.get(cv2.CAP_PROP_FPS)
+        print(f"fps: {fps}")
         time_per_frame = 1/fps
-        start_time = 0
+        start_time = "00:00:00"
         counting = False
         prev_frame = None
         similar = None
@@ -32,8 +35,9 @@ if __name__ == '__main__':
         print(frame_width, frame_height)
         # Define the codec and create VideoWriter object.The output is stored in 'outpy.avi' file.
         start_index = 0
+        os.makedirs(f'{root_folder}/done', exist_ok=True)
         file_out = f"{root_folder}/done/{os.path.splitext(file)[0]}.mp4"
-        out = cv2.VideoWriter(file_out, cv2.VideoWriter_fourcc('m', 'p', '4', 'v'), fps, (frame_width, frame_height))
+        # out = cv2.VideoWriter(file_out, cv2.VideoWriter_fourcc('M','J','P','G'), fps, (frame_width, frame_height))
 
         scale_percent = 10  # percent of original img size
         width = int(frame_width * scale_percent / 100)
@@ -42,6 +46,7 @@ if __name__ == '__main__':
         number_frame = 0
         is_full_sense = False
         total_frame = 0
+        times = []
         # Read until video is completed
         while cap.isOpened():
             # Capture frame-by-frame
@@ -51,40 +56,65 @@ if __name__ == '__main__':
                 current_time = datetime.datetime.utcfromtimestamp(total_frame/fps).strftime("%H:%M:%S")
                 # Display the resulting frame
 
-                if prev_frame is not None:
-                    frame = cv2.resize(frame, dim, interpolation=cv2.INTER_AREA)
-                    # print(frame.shape)
-                    # prev_img = cv2.resize(prev_frame, dim, interpolation=cv2.INTER_AREA)
-                    if prev_frame.shape[0] == 108:
-                        similar = ssim(frame, prev_frame)
-                        # prev_frame_gray = cv2.cvtColor(prev_frame, cv2.COLOR_BGR2GRAY)
-                        # original_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-                        # similar = compare_images(frame, prev_frame, 'demo')
-                        print("SSIM: %.2f FILE_OUT: %s Time %.1f" % (similar, file_out, round(total_frame/(fps*60), 1)))
-                prev_frame = frame
+                frame_resize = cv2.resize(frame, dim, interpolation=cv2.INTER_AREA)
+                # print(frame.shape)
+                # prev_img = cv2.resize(prev_frame, dim, interpolation=cv2.INTER_AREA)
+                if type(prev_frame) == np.ndarray:
+                    similar = ssim(frame_resize, prev_frame)
+                    # prev_frame_gray = cv2.cvtColor(prev_frame, cv2.COLOR_BGR2GRAY)
+                    # original_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+                    # similar = compare_images(frame, prev_frame, 'demo')
+                    # print("SSIM: %.2f FILE_OUT: %s Time %.1f" % (similar, file_out, round(total_frame/(fps*60), 1)))
+                prev_frame = frame_resize
 
                 if similar:
-                    if similar >= 0.9 and number_frame/fps < 10:
+                    if similar >= 0.85:
+                        # if number_frame/fps < 5:
                         number_frame += 1
-                        if number_frame == 1:
-                            print(f"start counting {current_time}")
+                        # print(number_frame)
+                        if number_frame == 1 and not counting:
+                            # print(f"start counting {current_time}")
                             counting = True
-                        out.write(frame)
-                        # cv2.imshow('Frame', frame)
+                            # out.write(frame)
+                            # cv2.imshow('Frame', frame_resize)
+                        elif number_frame == 150 and counting:
+                            counting = False
+                            times.append([start_time, current_time])
+                            print(f"{start_time}, {current_time}")
                     if similar < 0.85:
-                        if counting == True:
+                        if counting:
                             counting = False
                             # print(f"end counting {current_time}")
+                        start_time = current_time
                         number_frame = 0
-                        print(total_frame/fps)
+                        # print(total_frame/fps)
 
                 total_frame += 1
                 # Press Q on keyboard to  exit
-                if cv2.waitKey(25) & 0xFF == ord('q'):
-                    break
+                # cv2.waitKey(1)
+            else:
+                break
 
+        # times = [["00:00:00", get_duration(name)]]
+        open('concatenate.txt', 'w').close()
+        for idx, time in enumerate(times):
+            os.makedirs('tmp', exist_ok=True)
+            output_filename = f"tmp/output{idx}.mp4"
+            cmd = ["ffmpeg", "-i", filename, "-ss", time[0], "-to", time[1], "-f", "mp4", output_filename, "-y"]
+            subprocess.check_output(cmd)
+
+            with open("concatenate.txt", "a") as myfile:
+                myfile.write(f"file {output_filename}\n")
+
+        cmd = ["ffmpeg", "-f", "concat", "-i", "concatenate.txt", "-c", "copy", file_out, "-y"]
+        output = subprocess.check_output(cmd).decode("utf-8").strip()
+
+        for file_tmp in os.listdir('tmp'):
+            os.remove(f"tmp/{file_tmp}")
+        os.remove('concatenate.txt')
         # When everything done, release the video capture object
         cap.release()
+        # out.release()
 
         # Closes all the frames
         cv2.destroyAllWindows()
