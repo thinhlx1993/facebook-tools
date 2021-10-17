8416ad683e3fc0047ec5c73c4e91408eee5a6e17
ThinhPC <thinh.le@boot.ai>
2021-10-17 09:54:24 +0700

    save

--- script/crawl.py
+++ script/crawl.py
@@ -1,14 +1,14 @@
 from bs4 import BeautifulSoup
 import os
 
-out_put = "links/fb.txt"
+out_put = "links/Movies HD Plus.txt"
 
 # if os.path.isfile(out_put):
 #     os.remove(out_put)
 
 
-page_name = "Creative workz 2022 _ Facebook.html"
-html_doc = open(f"../template/{page_name}", encoding="utf-8")
+page_name = "Movies HD Plus _ Facebook.html"
+html_doc = open(f"C:\\Users\\HOMEPC\\Downloads\\{page_name}", encoding="utf-8")
 soup = BeautifulSoup(html_doc, 'html.parser')
 
 for parent in soup.find_all(class_='n851cfcs'):
@@ -27,16 +27,16 @@ for parent in soup.find_all(class_='n851cfcs'):
             # if "M" in view_count:
             #     view_count_float = view_count.replace("M", "").replace("Views", "")
             #     view_count_float = float(view_count_float)
-            #     if view_count_float > 1:
+            #     # if view_count_float > 1:
             with open(out_put, 'a') as file:
                 file.write(f"{href}-{view_count}\n")
                 file.close()
-            print(href, view_count)
+                print(href, view_count)
             # elif "K" in view_count:
             #     view_count = view_count.replace("K", "").replace("Views", "")
             #     view_count = float(view_count)
-            #     if view_count > 100:
+            #     if view_count > 8:
             #         print(href, view_count)
-            #         with open(f"input.txt", 'a') as file:
+            #         with open(out_put, 'a') as file:
             #             file.write(f"{href}-{view_count}K Views \n")
             #             file.close()
--- script/download.py
+++ script/download.py
@@ -1,16 +1,11 @@
 import youtube_dl
 import os
 
-import argparse
-parser = argparse.ArgumentParser()
-parser.add_argument("filename")
-args = parser.parse_args()
+page_name = "input.txt"
+os.makedirs(f"downloaded", exist_ok=True)
 
-page_name = args.filename
-os.makedirs(f"downloaded/{page_name}", exist_ok=True)
-
-if os.path.isfile(f"links/{page_name}.txt"):
-    with open(f"links/{page_name}.txt") as file:
+if os.path.isfile(f"{page_name}.txt"):
+    with open(f"{page_name}.txt") as file:
         for line in file.readlines():
             link, views = line.split('-')
             views = views.replace('\n', '')
@@ -20,7 +15,7 @@ if os.path.isfile(f"links/{page_name}.txt"):
                     info_dict = ydl.extract_info(link, download=False)
                     video_title = info_dict.get('title', None)
                     ext = info_dict.get('ext', None)
-                    ydl_opts = {'outtmpl': f'downloaded/{page_name}/{views}-{video_title}'}
+                    ydl_opts = {'outtmpl': f'downloaded/{views}-{video_title}'}
                     with youtube_dl.YoutubeDL(ydl_opts) as ydl:
                         ydl.download([link])
                 except Exception as ex:
